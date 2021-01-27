36e1279e3a08b35b549d2822e8948ec9688e0e64
Guilherme Carvalho <guilherme.carvalho@nuvols.com.br>
2021-01-27 14:34:36 -0300

    Add logout view

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from nuvols.core.views import (IndexAdminTemplateView, LoginView, ProfileView, ProfileUpdateView,
+from nuvols.core.views import (IndexAdminTemplateView, LoginView, LogoutView, ProfileView, ProfileUpdateView,
                                UpdatePassword, ResetPassword, SettingsView)
 
 app_name = 'core'
 urlpatterns = [
     path('', IndexAdminTemplateView.as_view(), name='index'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/update/', ProfileUpdateView.as_view(), name='profile-update'),
     path('profile/reset/password/', ResetPassword.as_view(), name='reset-password'),
